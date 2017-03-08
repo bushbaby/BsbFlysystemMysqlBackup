d2aefef48c2c914d7bef2d372ff6175568674626
Bas Kamer <bas@bushbaby.nl>
2017-03-08 10:22:23 +0100

    cs

--- config/bsb-offsite-mysql-backup.expressive.php
+++ config/bsb-offsite-mysql-backup.expressive.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 use BsbDoctrineRegistry\Container\ManagerRegistryFactory;
 use BsbFlysystemMysqlBackup\Container\MysqlBackupServiceFactory;
 use BsbFlysystemMysqlBackup\Container\MysqlBackupServiceFactory;
--- config/bsb-offsite-mysql-backup.php
+++ config/bsb-offsite-mysql-backup.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 use Ifsnop\Mysqldump\Mysqldump;
 
 return [
